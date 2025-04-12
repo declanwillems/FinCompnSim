39591377ba934042896c371b39ec203a8b652bef
declanwillems <declanwillems@gmail.com>
2025-04-12 15:01:05 -0400

    Part 1 and Start of Part 2
    * Implied Volatility Surface function added
    * Function is iterative for any number of tickers
    * Simulated GBM in GBM_sim function

--- FinCompnSimProject.R
+++ FinCompnSimProject.R
@@ -6,6 +6,11 @@
 
 rm(list = ls())
 
+library("plotly")
+# Package to connect points in scatter plot to make a surface
+# Utilized in part 1 for IV plot
+library("akima")
+
 
 setwd("C:\\Users\\willed3\\OneDrive - Rensselaer Polytechnic Institute\\Documents\\Spring 2025\\Financial Computation & Simulation\\Project\\Data")
 
@@ -41,5 +46,69 @@ stock_filtered <- na.omit(stock_filtered)
 options_filtered <- na.omit(options_filtered)
 
 
+# Implied Volatility Surfaces from observed option prices for all stocks
+
+
+options_filtered$Trade.Date <- as.Date(options_filtered$Trade.Date)
+
+options_filtered$Expiry.Date <- as.Date(options_filtered$Expiry.Date)
+
+options_filtered$TTM <- as.numeric(difftime(options_filtered$Expiry.Date, options_filtered$Trade.Date, units = "days")) / 365
+
+
+plot_IV_surface <- function(options_data, tickers){
+  
+  
+  for (ticker in tickers){
+    
+    iter_data <- subset(options_data, Stock.Ticker == ticker)
+    
+    
+    
+    interpolate_grid <- akima::interp(
+      
+      x = iter_data$Strike,
+      y = iter_data$TTM,
+      z = iter_data$Avg.IV,
+      duplicate = "mean", # if multiple points are at same point
+      linear = TRUE
+    )
+    
+    plot_inputs <- plot_ly(
+      
+      x = interpolate_grid$x,
+      y = interpolate_grid$y,
+      z = interpolate_grid$z,
+      type = "surface"
+      
+    ) %>%
+      layout(
+        title = paste("IV Surface:", ticker),
+        scene = list(
+        xaxis = list(title = "Strike"),
+        yaxis = list(title = "Time to Maturity"),
+        zaxis = list(title = "Avg. IV")
+        )
+      )
+    print(plot_inputs)
+  }
+}
+
+
+plot_IVs <- plot_IV_surface(options_filtered, ticker_choices)
+
+
+# Part 2
 # Build GBM Model
+
+GBM_sim <- function(S0, K, r, t, sigma, N, M){
+  
+  
+  dt <- t / N
+  
+  time_vec <- seq(0, t, length = N + 1)
+  
+  
+  
+}
 
